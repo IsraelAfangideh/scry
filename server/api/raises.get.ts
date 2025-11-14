e0133ea53122be308a0bc63225ad0db3f68fbc96
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2025-11-14 18:23:12 +0100

    Create server endpoint: raises.get.ts

---
+++ server/api/raises.get.ts
@@ -0,0 +1,79 @@
+export default defineEventHandler(async (event) => {
+    try {
+        const config = useRuntimeConfig()
+        const apiKey = config.kingsApiKey;
+        const base = config.kingsApiBase;
+
+        if (!apiKey || !base) {
+            throw createError({
+                statusCode: 500,
+                statusMessage: "Missing KingsCrowd API configuration"
+            })
+        }
+
+        const query = getQuery(event);
+        const page = query.page ? Number(query.page) : 1;
+        const limit = query.limit ? Number(query.limit) : 25;
+
+        const filters = [
+            {
+                key: 'raise_status',
+                symbol: '=',
+                value: ['Active'],
+            },
+            {
+                key: 'crowdfunding_type_1',
+                symbol: '=',
+                value: ['RegCF', 'RegA+'],
+            }
+        ]
+        const url = new URL("/api/v1/deals", base);
+
+        url.searchParams.set('page', String(page));
+        url.searchParams.set('limit', String(limit));
+        url.searchParams.set('filters', JSON.stringify(filters));
+        url.searchParams.set('order_by', 'start_date')
+        url.searchParams.set('sort', 'desc')
+
+
+        const res = await fetch(url.toString(), {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+                Accept: 'application/json',
+            },
+        })
+
+        if (!res.ok) {
+            const errorText = await res.text()
+            throw createError({
+                statusCode: res.status,
+                statusMessage: `KingsCrowd API Error: ${errorText}`,
+            })
+        }
+
+        const json = await res.json()
+
+        if (!json?.data?.result) {
+            throw createError({
+                statusCode: 500,
+                statusMessage: `KingsCrowd API Error: Malformed Response`,
+            })
+        }
+
+        return {
+            ok: true,
+            pagination: {
+                count: json.data.count,
+                totalCount: json.data.total_count,
+                totalPages: json.data.total_pages,
+                currentPage: json.data.current_page,
+            },
+            results: json.data.result,
+        }
+    } catch (err: any) {
+        throw createError({
+            statusCode: err.statusCode || 500,
+            statusMessage: err.statusMessage || err.message || "Server Error"
+        })
+    }
+})
