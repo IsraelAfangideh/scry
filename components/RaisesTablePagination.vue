a3ebdf66c0397a80be44463f5902dca16bc64532
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2025-11-17 11:30:52 +0100

    Fix pagination bug in RaisesTablePagination: adjust `canGoNext` logic to handle API inconsistency with total pages and limit displayed total pages to 20.

--- components/RaisesTablePagination.vue
+++ components/RaisesTablePagination.vue
@@ -7,7 +7,6 @@ const props = defineProps<{
   pagination: Pagination | null
 }>()
 
-const totalPages: number = Number(props.pagination?.totalPages)
 
 const emits = defineEmits<{
   (e: 'next-page'): void
@@ -15,7 +14,10 @@ const emits = defineEmits<{
   (e: 'set-limit'): number
 }>()
 
-const canGoNext = computed(() => props?.pagination && props?.pagination.currentPage < totalPages)
+// This is necessary because of a bug in the external API where the total pages value becomes 28 or 29 when we fetch page 20, but then if we try to fetch page 21 total pages becomes 0
+const paginationBug = computed(() => props?.pagination && props?.pagination.currentPage >= 20 && props.pagination.totalPages > 20)
+
+const canGoNext = computed(() => (props?.pagination && props?.pagination.currentPage < props.pagination.totalPages && !paginationBug.value))
 
 const canGoPrev = computed(() => (props?.pagination?.currentPage ?? 0) > 1)
 </script>
@@ -23,7 +25,7 @@ const canGoPrev = computed(() => (props?.pagination?.currentPage ?? 0) > 1)
 <template>
   <div class="flex justify-between p-4 text-xs items-center text-slate-400">
     <div v-if="pagination" class="justify-center">
-      Page {{ pagination.currentPage }} out of {{ totalPages }}
+      Page {{ pagination.currentPage }} out of {{ Math.min(pagination.totalPages, 20) }}
     </div>
     <div class="flex space-x-5 justify-between">
       <button v-if="canGoPrev"
